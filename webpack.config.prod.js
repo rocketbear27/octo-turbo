e127c5f52e13d7119cb3189d04d62ddef9468172
RocketBear27 <rocketbear27.code@gmail.com>
2018-08-12 17:47:21 -0700

    Caching Files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
     debug: true,
@@ -14,9 +16,15 @@ export default {
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        filename: '[name].js'
+        filename: '[name].[chunkhash].js'
     },
     plugins: [
+        // Generate an external css file with a hash in the filename
+        new ExtractTextPlugin('[name].[contenthash].css'),
+
+        // Hash the files using MD5 so that their names change when the content changes.
+        new WebpackMd5Hash(),
+
         // Use CommonsChunkPlugin to create a separate bundle
         // of vendor libraries so that they're cached separately.
         new webpack.optimize.CommonsChunkPlugin({
@@ -43,7 +51,7 @@ export default {
             },
             {
                 test: /\.css$/,
-                loaders: ['style', 'css']
+                loader: ExtractTextPlugin.extract('css?sourceMap')
             }
         ]
     }
